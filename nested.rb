c3c9186dc4d63bcff8af745d026f66f1eb6ba68f
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-03 14:28:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,6 +84,8 @@ def adding_matz
      }
   programmer_hash[:yukihiro_matsumoto] = {}
   :yukihiro_matsumoto[:known_for] = "Ruby"
+  :yukihiro_matsumoto[:languages] = ["LISP", "C"]
+  programmer_hash
     
 end
 
